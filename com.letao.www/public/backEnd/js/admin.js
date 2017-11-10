86a5b1141e387595d3820d3921e758a9ecaf1052
taoxiaoyao09 <751587041@qq.com>
2017-11-10 18:10:54 +0800

    exit login

--- com.letao.www/public/backEnd/js/admin.js
+++ com.letao.www/public/backEnd/js/admin.js
@@ -1,1 +1,29 @@
 // 该文件的功能是用来写首页的js交互的
+$(function(){
+  // 侧边栏隐藏显示
+  $("[data-menu]").on("click",function(){
+    $(".menu").toggle();
+    $(".content").toggleClass("menuMove");
+  })
+})
+// 登录退出功能
+$("[data-logout]").on("click",function(){
+  $('#loginModal').modal('toggle');
+  // 取消
+  $("#cancel").model("hide");
+  // 退出登录
+  $("#exit").on("click",function(){
+    $.ajax({
+      type:"get",
+      url:"login.html",
+      date:{},
+      dateType:"json",
+      success:function(date){
+        if(data.success){
+          /*7.退出成功*/
+          location.href = 'login.html';
+      }
+      }
+    })
+  })
+})
