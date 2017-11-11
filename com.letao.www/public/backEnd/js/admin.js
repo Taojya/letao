4878af48a85c3d3494a85250777b9ccaada85b1d
taoxiaoyao09 <751587041@qq.com>
2017-11-11 13:34:02 +0800

    login.html commit

--- com.letao.www/public/backEnd/js/admin.js
+++ com.letao.www/public/backEnd/js/admin.js
@@ -1,5 +1,5 @@
 // 该文件的功能是用来写首页的js交互的
-$(function () {
+$(function ($) {
   // 侧边栏隐藏显示
   $("[data-menu]").on("click", function () {
     $(".menu").toggle();
@@ -19,20 +19,22 @@ $(function () {
   // 登录退出功能
   $("[data-logout]").on("click", function () {
     $('#loginModal').modal('toggle');
-    // 取消
-    $("#cancel").model("hide");
     // 退出登录
     $("#exit").on("click", function () {
+      console.log(1);
       $.ajax({
         type: "get",
-        url: "login.html",
+        url: "/employee/employeeLogout",
         date: {},
         dateType: "json",
         success: function (date) {
-          if (data.success) {
-            /*7.退出成功*/
-            location.href = 'login.html';
-          }
+          setTimeout(function () {
+            if (data.success) {
+              /*7.退出成功*/
+              location.href = 'login.html';
+            }
+          }, 500)
+
         }
       })
     })
