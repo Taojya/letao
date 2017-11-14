f9d244509c6c003a574293f7c706ad354e4981f8
taoxiaoyao09 <751587041@qq.com>
2017-11-14 21:56:47 +0800

    mobile commit

--- com.letao.www/public/backEnd/js/userManage.js
+++ com.letao.www/public/backEnd/js/userManage.js
@@ -1,5 +1,19 @@
 $(function($){
+// 获取数据，并呈现
+$getUserManage=function(pageNum){
+  $.ajax({
+    type:"get",
+    url:"/user/queryUser",
+    date:{
+      page:pageNum||1,
+      pageSize:5
+    },
+    success:function(date){
+      console.log(date);
+    }
 
+  })
+}
 
   
 })
